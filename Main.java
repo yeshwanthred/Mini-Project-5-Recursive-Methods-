ad4e041d1925481f8ac59060d7e895b63baacab7
yeshwanthred <71831250+yeshwanthred@users.noreply.github.com>
2021-02-22 12:45:32 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,6 +5,16 @@ class Main {
  public static void main(String[] args) {
 
     //Scanner Statement 
+    
+    Scanner scan = new Scanner(System.in);
+     System.out.println("Hey there, welcome to garage 4A.");
+     System.out.println("How many cars are in here?");
+
+     int carsAm = scan.nextInt();
+
+     System.out.println ("You have " + carsAm + " cars.");
+
+    
 
  }
 }
